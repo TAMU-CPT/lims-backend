61b75ef3b67d297c15bb33f2111e974008df6ee7
Eric Rasche <rasche.eric@gmail.com>
2016-04-20 19:34:37 +0000

    Add admin sites

--- directory/admin.py
+++ directory/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from directory.models import PersonTag, Organisation, Person
 
+admin.site.register(PersonTag)
+admin.site.register(Organisation)
+admin.site.register(Person)
 # Register your models here.
--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from web.models import ContainerType
+from web.models import TubeType
+from web.models import SampleType
+from web.models import StorageLocation
+from web.models import Box
+from web.models import Tube
+from web.models import EnvironmentalSample
+from web.models import Bacteria
+from web.models import Lysate
+from web.models import Experiment
+from web.models import ExperimentalResult
+from web.models import PhageDNAPrep
+from web.models import SequencingRunPool
+from web.models import SequencingRun
+from web.models import Assembly
+
+admin.site.register(ContainerType)
+admin.site.register(TubeType)
+admin.site.register(SampleType)
+admin.site.register(StorageLocation)
+admin.site.register(Box)
+admin.site.register(Tube)
+admin.site.register(EnvironmentalSample)
+admin.site.register(Bacteria)
+admin.site.register(Lysate)
+admin.site.register(Experiment)
+admin.site.register(ExperimentalResult)
+admin.site.register(PhageDNAPrep)
+admin.site.register(SequencingRunPool)
+admin.site.register(SequencingRun)
+admin.site.register(Assembly)
