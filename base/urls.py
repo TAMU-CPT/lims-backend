3ed33c85e444e0c803f61238cdc47996a401ce2a
Eric Rasche <rasche.eric@gmail.com>
2016-08-17 16:24:04 +0000

    Move into API app

--- base/urls.py
+++ base/urls.py
@@ -1,18 +1,7 @@
 from django.conf.urls import url, include
-from views import Index, AppViewSet
-from rest_framework import routers
+from views import Index
 
-router = routers.DefaultRouter()
-router.register(r'apps', AppViewSet)
-
-# from web import views
 
 urlpatterns = [
     url(r'^$', Index.as_view(), name='index'),
-
-    url(r'api/', include(router.urls)),
-    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # url(r'api-token-auth/','rest_framework_jwt.views.obtain_jwt_token'),
-    # url(r'api-token-refresh/', 'rest_framework_jwt.views.refresh_jwt_token'),
-    # url(r'api-token-verify/', 'rest_framework_jwt.views.verify_jwt_token'),
 ]
