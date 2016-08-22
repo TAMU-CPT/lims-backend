f8c8bb65219da994a2d1eba87999d84818b02bfd
Eric Rasche <rasche.eric@gmail.com>
2016-08-22 20:21:29 +0000

    Expose more data

--- directory/serializers.py
+++ directory/serializers.py
@@ -1,6 +1,6 @@
 from directory.models import Organisation
 from account.models import Account
-
+import hashlib
 from rest_framework import serializers
 
 
@@ -10,23 +10,28 @@ class AccountLessOrgSerializer(serializers.ModelSerializer):
         model = Organisation
         fields = (
             'name', 'emails', 'phone_number', 'fax_number',
-            'street_address', 'website'
+            'street_address', 'website', 'id'
         )
 
 class AccountSerializer(serializers.ModelSerializer):
     orgs = serializers.SerializerMethodField()
+    gravatar_email = serializers.SerializerMethodField()
 
     class Meta:
         model = Account
         fields = (
             'user', 'timezone', 'language', 'name', 'initials',
             'nickname', 'netid', 'phone_number', 'tags', 'orcid', 'orgs',
-            'original_id'
+            'original_id', 'gravatar_email'
         )
 
     def get_orgs(self, obj):
         for org in obj.orgs.all():
             yield AccountLessOrgSerializer(org).data
+
+    def get_gravatar_email(self, obj):
+        for email in obj.emails():
+            return hashlib.md5(email.email).hexdigest()
 
 
 class OrgLessUserSerializer(serializers.ModelSerializer):
