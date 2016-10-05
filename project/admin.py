5c8c17c13a88fb0f5af4f7ed809dfd63dcf6668c
Eric Rasche <rasche.eric@gmail.com>
2016-10-05 18:08:29 +0000

    Simple permission model on projects

--- project/admin.py
+++ project/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 
-from project.models import Bioproject
+from project.models import Bioproject, EditingRoleUser, EditingRoleGroup
+
 admin.site.register(Bioproject)
+admin.site.register(EditingRoleUser)
+admin.site.register(EditingRoleGroup)
--- project/models.py
+++ project/models.py
@@ -2,10 +2,57 @@ from __future__ import unicode_literals
 
 from django.db import models
 from web.models import Phage
+from django.contrib.auth.models import User, Group
 # Create your models here.
+
+ROLES = (
+    (0, 'Viewer'),
+    (1, 'Editor'),
+    (2, 'Administrator')
+)
+
+ROLES_VERBS = {
+    0: 'view',
+    1: 'edit',
+    2: 'administer',
+}
+
 
 class Bioproject(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField(blank=True)
     sample = models.ManyToManyField(Phage, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    # Can change project sharing
+    access_user = models.ManyToManyField(User, blank=True, through='EditingRoleUser')
+    access_group = models.ManyToManyField(Group, blank=True, through='EditingRoleGroup')
+
+    def __str__(self):
+        return self.name
+
+
+class EditingRoleUser(models.Model):
+    user = models.ForeignKey(User)
+    bioproject = models.ForeignKey(Bioproject)
+    role = models.IntegerField(choices=ROLES)
+
+    def __str__(self):
+        return '%s can %s %s' % (
+            self.user.username,
+            ROLES_VERBS[self.role],
+            self.bioproject
+        )
+
+
+class EditingRoleGroup(models.Model):
+    group = models.ForeignKey(Group)
+    bioproject = models.ForeignKey(Bioproject)
+    role = models.IntegerField(choices=ROLES)
+
+    def __str__(self):
+        return '%s can %s %s' % (
+            self.group.name,
+            ROLES_VERBS[self.role],
+            self.bioproject
+        )
