b4f327342d1ac55e4122309b0aa40341482b8945
Eric Rasche <rasche.eric@gmail.com>
2016-10-05 17:59:54 +0000

    allow empty sample list

--- project/models.py
+++ project/models.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from web.models import EnvironmentalSample
+from web.models import Phage
 # Create your models here.
 
 class Bioproject(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField(blank=True)
-    sample = models.ManyToManyField(EnvironmentalSample)
+    sample = models.ManyToManyField(Phage, blank=True)
     date = models.DateTimeField(auto_now_add=True)
