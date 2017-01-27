f6fa4515f4af306683820b80126e45d35a756884
Eric Rasche <rasche.eric@gmail.com>
2017-01-27 03:40:39 +0000

    URL prefix properly

--- base/urls.py
+++ base/urls.py
@@ -1,15 +1,18 @@
+import os
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^account/', include('account.urls')),
-    url(r'^lims/', include('lims.urls')),
-    url(r'^bioproject/', include('bioproject.urls')),
-    url(r'^directory/', include('directory.urls')),
-    url(r'^api-auth/', include('rest_framework.urls')),
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
+    url(os.environ.get('DJANGO_URL_PREFIX', ''), include([
+        url(r'^admin/', admin.site.urls),
+        url(r'^account/', include('account.urls')),
+        url(r'^lims/', include('lims.urls')),
+        url(r'^bioproject/', include('bioproject.urls')),
+        url(r'^directory/', include('directory.urls')),
+        url(r'^api-auth/', include('rest_framework.urls')),
+        url(r'^api-token-auth/', obtain_jwt_token),
+        url(r'^api-token-verify/', verify_jwt_token),
+    ])),
 ]
